81fd2746edacbd1390c4bbb6c09ffbe7716fede7
SungYeobKim <49201228+yeobi27@users.noreply.github.com>
2025-07-01 17:58:05 +0900

    add product crud 2

--- src/main/java/com/sparta/java_02/domain/product/dto/ProductResponse.java
+++ src/main/java/com/sparta/java_02/domain/product/dto/ProductResponse.java
@@ -1,5 +1,7 @@
 package com.sparta.java_02.domain.product.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -13,15 +15,18 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // null 필드는 JSON에서 제외
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductResponse {
 
-  private Long id;
-  private Long categoryId;
-  private String name;
-  private String description;
-  private BigDecimal price;
-  private Integer stock;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
+  Long id;
+  Long categoryId;
+  String name;
+  String description;
+  BigDecimal price;
+  Integer stock;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  LocalDateTime createdAt;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  LocalDateTime updatedAt;
 }
--- src/main/java/com/sparta/java_02/domain/purchase/dto/PurchaseResponse.java
+++ src/main/java/com/sparta/java_02/domain/purchase/dto/PurchaseResponse.java
@@ -1,5 +1,6 @@
 package com.sparta.java_02.domain.purchase.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.math.BigDecimal;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // null 필드는 JSON에서 제외
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PurchaseResponse {
 
