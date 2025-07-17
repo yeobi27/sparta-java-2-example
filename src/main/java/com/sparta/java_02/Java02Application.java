1ba708310b77848829a7d2772437b3bc141e00bb
yeobi27 <49201228+yeobi27@users.noreply.github.com>
2025-07-18 01:04:34 +0900

    feat EXPLAIN

--- src/main/java/com/sparta/java_02/Java02Application.java
+++ src/main/java/com/sparta/java_02/Java02Application.java
@@ -3,10 +3,12 @@ package com.sparta.java_02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 // 내부적으로 비동기를 쓰려면 꼭 추가해줘야하는 @EnableAsync
 @EnableAsync
+@EnableScheduling // 스케줄링 기능을 활성화합니다.
 @EnableRedisHttpSession
 @SpringBootApplication
 public class Java02Application {
